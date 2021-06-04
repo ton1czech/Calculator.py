f79ca70847c449779015a0640d406b2fabc64766
ton1czech <tonyasek007@gmail.com>
2021-06-05 00:21:31 +0200

    🎁NEW: adding hell easter egg

--- src/operations.py
+++ src/operations.py
@@ -4,21 +4,31 @@ def _addition(base_num, *nums):
     for num in nums:
         res += num
     print(f"{res}")
+    __hell(res)
 
 def _subtraction(base_num, *nums):
     res = base_num
     for num in nums:
         res -= num
     print(f"{res}")
+    __hell(res)
 
 def _multiplication(base_num, *nums):
     res = base_num
     for num in nums:
         res *= num
     print(f"{res}")
+    __hell(res)
 
 def _division(base_num, *nums):
     res = base_num
     for num in nums:
         res /= num
     print(f"{res}")
+    __hell(res)
+
+def __hell(res):
+    if res == 666:
+        print("See you in hell")
+    else:
+        pass
