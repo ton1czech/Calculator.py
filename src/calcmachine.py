9fcbbe84299f2ce262b40c0554efd255adf5754b
ton1czech <tonyasek007@gmail.com>
2021-06-17 21:49:36 +0200

    🎁NEW: error handling

--- src/calcmachine.py
+++ src/calcmachine.py
@@ -10,10 +10,28 @@ from curtsies.fmtfuncs import cyan, magenta, red, bold
 
 def calculate():
     while True:
-        base = float(input("Enter your first number: "))
-        opr = input(f"Enter Operator {bold('(+ - * / **)')}: ")
-
-        nums = [float(i) for i in input("Enter another number/s (n1,n2): ").split(",")]
+        while True:
+            base = None
+            try:
+                base = float(input("Enter your first number: "))
+            except ValueError:
+                print(red("Not a number"))
+            if base:
+                break
+        while True:
+            opr = input(f"Enter Operator {bold('(+ - * / **)')}: ")
+            if opr == "+" or opr == "-" or opr == "*" or opr == "/" or opr == "**":
+                break
+            else:
+                print(red("Wrong Operator!"))
+        while True:
+            nums = None
+            try:
+                nums = [float(i) for i in input("Enter another number/s (n1,n2): ").split(",")]
+            except ValueError:
+                print(red("Check your formatting. Something is wrong"))
+            if nums:
+                break
 
         if opr == '+':
             op.addition(opr, base, *nums)
@@ -25,11 +43,9 @@ def calculate():
             op.division(opr, base, *nums)
         elif opr == '**':
             op.exponent(opr, base, nums[0])
-        else:
-            print(red("Wrong Operator!"))
 
-        more = input(magenta("Do you want to calculate more? (Y/n): "))
-        if more == "n" or more == "no" or more == 'No':
+        more = input(magenta("Do you want to calculate more? (Y/n): ")).lower()
+        if more == "n" or more == "no":
             print(cyan("Sad to see you go. See you soon."))
             break
 
