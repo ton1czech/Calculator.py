b2659c34bd16b9f0a2d0ca389ef412fb4756a3f5
ton1czech <tonyasek007@gmail.com>
2021-06-19 02:18:53 +0200

    👌IMPROVE: one function for all operators

--- src/calcmachine.py
+++ src/calcmachine.py
@@ -3,17 +3,14 @@
 # 28 09 2020                        #
 #####################################
 
-import operations as op
-from curtsies.fmtfuncs import cyan, magenta, red, bold
+from curtsies.fmtfuncs import cyan, magenta, red, bold, green
 
-
-
-def calculate():
+def main():
     while True:
         while True:
             base = None
             try:
-                base = float(input("Enter your first number: "))
+                base = input("Enter your first number: ")
             except ValueError:
                 print(red("Not a number"))
             if base:
@@ -27,27 +24,26 @@ def calculate():
         while True:
             nums = None
             try:
-                nums = [float(i) for i in input("Enter another number/s (n1,n2): ").split(",")]
+                nums = [i for i in input("Enter another number/s (n1,n2): ").split(",")]
             except ValueError:
                 print(red("Check your formatting. Something is wrong"))
             if nums:
                 break
 
-        if opr == '+':
-            op.addition(opr, base, *nums)
-        elif opr == '-':
-            op.subtraction(opr, base, *nums)
-        elif opr == '*':
-            op.multiplication(opr, base, *nums)
-        elif opr == '/':
-            op.division(opr, base, *nums)
-        elif opr == '**':
-            op.exponent(opr, base, nums[0])
+        calculate(opr, base, nums)
 
         more = input(magenta("Do you want to calculate more? (Y/n): ")).lower()
         if more == "n" or more == "no":
             print(cyan("Sad to see you go. See you soon."))
             break
 
+def calculate(opr, base, *nums):
+    print(f"\n{base}", end="")
+    for num in nums[0]:
+        base = base + opr + num
+        res = eval(base)
+        print(f" {opr} {num}", end="")
+    print(" = ", end=""), print(bold(green(f"{res}\n")))
+
 if __name__ == "__main__":
-    calculate()
+    main()
