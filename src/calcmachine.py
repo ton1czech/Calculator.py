0275595f94585fd946bec5e8105e4b8e8b46bb27
ton1czech <tonyasek007@gmail.com>
2021-06-19 10:10:10 +0200

    👌IMPROVE: error handling

--- src/calcmachine.py
+++ src/calcmachine.py
@@ -8,27 +8,27 @@ from curtsies.fmtfuncs import cyan, magenta, red, bold, green
 def main():
     while True:
         while True:
-            base = None
-            try:
-                base = input("Enter your first number: ")
-            except ValueError:
-                print(red("Not a number"))
-            if base:
+            base = input("Enter your first number: ").strip()
+            if base.isalpha() or base == "" or not base.isalnum():
+                print(red("Not a number!"))
+            else:
                 break
         while True:
-            opr = input(f"Enter Operator {bold('(+ - * / **)')}: ")
-            if opr == "+" or opr == "-" or opr == "*" or opr == "/" or opr == "**":
+            opr = input(f"Enter Operator {bold('([+] [-] [*] [/] [**] [//] [%])')}: ").strip()
+            if opr == "+" or opr == "-" or opr == "*" or opr == "/" or opr == "**" or opr == "//" or opr == "%":
                 break
             else:
                 print(red("Wrong Operator!"))
         while True:
-            nums = None
-            try:
-                nums = [i for i in input("Enter another number/s (n1,n2): ").split(",")]
-            except ValueError:
-                print(red("Check your formatting. Something is wrong"))
-            if nums:
+            nums = [i for i in input("Enter another number/s (n1,n2): ").strip().split(",")]
+            fails = 0
+            for num in nums:
+                if num.isalpha() or num == "":
+                    fails += 1
+            if fails == 0:    
                 break
+            else:
+                print(red("Can't calculate with non-numbers!"))
 
         calculate(opr, base, nums)
 
